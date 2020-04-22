b2d41cef499ec019496baff536d3bb3a8747e084
Cqh-i <1036051726@qq.com>
2020-04-22 12:05:53 +0800

    add:第二个小工具，获取配置文件的工具类

--- src/main/java/com/qunhuichen/utils/UtilsApplication.java
+++ src/main/java/com/qunhuichen/utils/UtilsApplication.java
@@ -1,12 +1,19 @@
 package com.qunhuichen.utils;
 
+import com.qunhuichen.utils.util.ConfigUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UtilsApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(UtilsApplication.class);
+
     public static void main(String[] args) {
+        logger.debug("this is debug");
+        logger.info("this is info");
         SpringApplication.run(UtilsApplication.class, args);
     }
 
